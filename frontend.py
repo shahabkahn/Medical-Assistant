bb88ec3fa349b3661e4d84c581d4478c8a2472ff
Shahab Khan <56758776+shahabkahn@users.noreply.github.com>
2024-05-21 15:12:05 +0500

    Update frontend.py

--- frontend.py
+++ frontend.py
@@ -5,10 +5,9 @@ import os
 
 load_dotenv()
 
-# Define the URL of your FastAPI endpoint
 url = "https://shahabkahn-medical-assistant.hf.space/query"
 
-# Initialize the session state
+# Initialize session state
 if "chat_history" not in st.session_state:
     st.session_state.chat_history = []
 if "user_input" not in st.session_state:
@@ -16,22 +15,18 @@ if "user_input" not in st.session_state:
 if "input_reset" not in st.session_state:
     st.session_state.input_reset = False
 
-# Function to handle the new chat button click
 def new_chat():
-    st.session_state.chat_history = []  # Clear the chat history
-    st.session_state.input_reset = True # Set the flag to reset the input
+    st.session_state.chat_history = []
+    st.session_state.input_reset = True
 
-# Streamlit app
 def app():
     st.title("Doctor's Medical Assistant")
     st.sidebar.button("New Chat", on_click=new_chat)
     st.image("2.jpg", width=300)
 
-    # Display Welcome message
     st.write("<span style='font-size:20px; font-weight:bold;'>Welcome! How Can I Help You</span>", 
              unsafe_allow_html=True)
 
-    # Placeholder text for the input box
     if st.session_state.input_reset:
         input_text = st.text_input("", placeholder="Enter your question here...", 
                                    key="user_input_reset", help="Type your question here...")
@@ -43,31 +38,24 @@ def app():
         input_text = st.text_input("", placeholder="Enter your question here...", 
                                    key="user_input", help="Type your question here...")
 
-    # Handle form submission
     submit_button = st.button("➡️")
     if submit_button:
         user_input = st.session_state.user_input.strip()
         if user_input:
-            # Create the request payload
             payload = {"question": user_input}
             try:
-                # Send the POST request to the FastAPI endpoint
                 response = requests.post(url, json=payload)
-                # Check if the request was successful
                 if response.ok:
-                    # Get the answer from the FastAPI endpoint
                     answer = response.json().get("answer")
                     st.session_state.chat_history.append({"role": "user", "content": user_input})
                     st.session_state.chat_history.append({"role": "assistant", "content": answer})
-                    # Clear the user input after submission
                     st.session_state.input_reset = True
-                    st.experimental_rerun()  # Rerun the app to clear the input field
+                    st.experimental_rerun()
                 else:
                     st.error(f"Error: {response.status_code} {response.text}")
             except requests.RequestException as e:
                 st.error(f"Error: {e}")
 
-    # Display chat history
     for chat in st.session_state.chat_history:
         if chat["role"] == "user":
             st.write(f"**You:** {chat['content']}")
